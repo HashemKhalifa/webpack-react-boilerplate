3ee613b9e759f3a14014f6c7b107e2c27992f27c
Hashem Khalifa <hashem.khalifa@tajawal.com>
2018-03-12 16:57:27 +0400

    Replace .js with jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-  entry: ['react-hot-loader/patch', './src/index.js'],
+  entry: ['react-hot-loader/patch', './src/index.jsx'],
   module: {
     rules: [
       {
