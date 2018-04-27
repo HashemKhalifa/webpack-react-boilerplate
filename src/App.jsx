9b589aa667166efc6080aede6dfbd43398fe65f4
Hashem Khalifa <hashem.khalifa@gmail.com>
2018-04-27 22:03:21 +0400

    update eslint along with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
-import HelloWorld from "./components/hello-world";
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import HelloWorld from './components/hello-world';
 
 class App extends Component {
   constructor(props) {
