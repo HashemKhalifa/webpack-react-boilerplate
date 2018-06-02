c805b9e2cb74f7f48e5dc270373b5d14149c610e
Hashem Khalifa <hashem.khalifa@gmail.com>
2018-06-02 23:30:47 +0400

    update pacakges and CSS module

--- src/components/hello-world/index.jsx
+++ src/components/hello-world/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './hello-world.css';
+import style from './hello-world.css';
 
-const HelloWorld = props => <div className="hello-world">{props.title}</div>;
+const HelloWorld = props => (
+  <div className={style['hello-world']}>{props.title}</div>
+);
 
 HelloWorld.propTypes = {
   title: PropTypes.string,
