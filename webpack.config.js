18b9214765ccc6444c83cf12af477ec05f0259fc
Hashem Khalifa <hashem.khalifa@gmail.com>
2018-06-16 21:25:33 +0400

    fix eslint error

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const envs = {
   development: 'dev',
   production: 'prod',
 };
+/* eslint-disable global-require,import/no-dynamic-require */
 const env = envs[process.env.NODE_ENV || 'development'];
 const envConfig = require(`./webpack/webpack.${env}.js`);
 module.exports = webpackMerge(common, envConfig);
