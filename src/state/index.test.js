8c6892bad2141cf4d7ebc2c6ba6a4eee24932aaa
Yuriy <yuriy.glebov@gmail.com>
2018-11-12 00:26:17 +0000

    Final touches

--- src/state/index.test.js
+++ src/state/index.test.js
@@ -8,9 +8,16 @@ describe('State reducer functions:: ', () => {
   })
   
   describe('Generate level:', () => {
+    state.score = 5;
     let newState = generateLevel(state);
     it ('Returns an object', () => {
       expect(newState).toBe(true);
-    })
-  })
+    });
+    it ('Generates array of given complexity', () => {
+      expect(newState.seq.length).toBe(5);
+    });
+    it ('Sets correct status', () => {
+      expect(newState.status).toBe('Watch');
+    });
+  });
 })
