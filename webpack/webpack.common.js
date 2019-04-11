00be62cb665cacab98a451b6949e136927be7486
Hashem Khalifa <hashem.khalifa@gmail.com>
2019-04-11 16:13:58 +0200

    remove babelrc and move all options inside webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -22,6 +22,37 @@ module.exports = {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /(node_modules)/,
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  esmodules: true,
+                },
+                useBuiltIns: 'usage',
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/plugin-transform-runtime',
+            'react-hot-loader/babel',
+
+            // Stage 2 https://github.com/babel/babel/tree/master/packages/babel-preset-stage-2
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            '@babel/plugin-proposal-function-sent',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-proposal-numeric-separator',
+            '@babel/plugin-proposal-throw-expressions',
+
+            // Stage 3
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-syntax-import-meta',
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            '@babel/plugin-proposal-json-strings',
+          ],
+        },
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
